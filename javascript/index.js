0bc7220512fe40bbe0f0373a6f12dd4954a512b1
sumayya <sumi.mea@gmail.com>
2022-06-17 20:39:11 +0100

    added javascript file

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,14 @@
-function addNumbers() {
-    
-     var num1=Number(document.getElementById('first').value)
-     var num2=Number(document.getElementById('second').value)
-     console.log(num1 + num2)
-     var sum = num1 + num2;
-
-
-document.getElementById('message').innerHTML="this is form javascript innerHTML"
+function addNumbers(){
+       var num1 = Number(document.getElementById('first').value)
+       var num2 = Number(document.getElementById('second').value)
+       console.log(num1 + num2)
+       var sum = num1 + num2
+       alert(sum)
+document.getElementById('message').innerHTML = 'The sum is : ' + sum
 }
+
+
+
+
+
+//console.log('hi from external js file')
--- javascript/welcome.js
+++ javascript/welcome.js
@@ -1,1 +1,4 @@
 console.log('welcome to node engine')
+console.log('welcome to google v8 engine')
+
+  
